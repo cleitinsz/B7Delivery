2027af476ff8110789ea384cef2156d10f581fba
cleitinsz <gabriel.playhard10@gmail.com>
2022-10-09 08:06:08 -0300

    third commit

--- pages/[tenant]/index.tsx
+++ pages/[tenant]/index.tsx
@@ -1,4 +1,5 @@
 import Banner from '../../components/Banner';
+import ProductItem from '../../components/ProductItem';
 import { SearchInput } from '../../components/SearchInput';
 import styles from '../../styles/Home.module.css'
 const Home = () => {
@@ -36,6 +37,35 @@ const Home = () => {
                 </div>
             </header>
             <Banner/>
+
+            <div className={styles.grid}>
+                <ProductItem
+                    data={{ id:1 , image: '/tmp/burguer.png', categoryName:' Tradicional', name: 'Texas Burger', price:' R$ 25,50'}}
+                    mainColor="fb9400"
+                    secondColor='#ccc'
+                />
+                <ProductItem
+                    data={{  id:2 ,image: '/tmp/burguer.png', categoryName:' Tradicional', name: 'Texas Burger', price:' R$ 25,50'}}
+                    mainColor="fb9400"
+                    secondColor='#ccc'
+                />
+                <ProductItem
+                    data={{  id:3 ,image: '/tmp/burguer.png', categoryName:' Tradicional', name: 'Texas Burger', price:' R$ 25,50'}}
+                    mainColor="fb9400"
+                    secondColor='#ccc'
+                />
+                <ProductItem
+                    data={{  id:4 ,image: '/tmp/burguer.png', categoryName:' Tradicional', name: 'Texas Burger', price:' R$ 25,50'}}
+                    mainColor="fb9400"
+                    secondColor='#ccc'
+                />
+                
+                <ProductItem
+                    data={{  id:5 ,image: '/tmp/burguer.png', categoryName:'Tradicional', name: 'Texas Burger', price:' R$ 25,50'}}
+                    mainColor="fb9400"
+                    secondColor='#ccc'
+                />
+            </div>
         </div>
     )
 }
