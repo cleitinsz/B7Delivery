5610717c943a7638925290e812a014fdb57b63e0
cleitinsz <gabriel.playhard10@gmail.com>
2022-10-14 14:42:36 -0300

    login

--- pages/[tenant]/login.tsx
+++ pages/[tenant]/login.tsx
@@ -1,6 +1,9 @@
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
+import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { InputField } from '../../components/InputField';
 import { useAppContext } from '../../contexts/AppContext';
@@ -16,8 +19,19 @@ useEffect(()=>{
 
     },[]);
 
+    const router = useRouter ();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    
+    const handleSubmit = () => {
+        
+    }
+
+    const handleSignUp = () => {
+        router.push(`/${data.tenant.slug}/signup`);
+    }
 
     return (
         <div className={styles.container}>
@@ -30,19 +44,61 @@ useEffect(()=>{
             backHref={`/${data.tenant.slug}`} 
             />
             
-            <InputField
-            color={data.tenant.mainColor}
-            placeholder="Digite seu e-mail"
-            value={email}
-            onChange={setEmail}
-            />
-            <InputField
-            color={data.tenant.mainColor}
-            placeholder="Digite sua senha"
-            value={password}
-            onChange={setPassword}
-            password
-            />
+            <div className={styles.header}>
+                {data.tenant.name}
+            </div>
+            <div 
+                className={styles.subtitle}
+                style={{borderBottomColor: data.tenant.mainColor}}
+            >Use suas credenciais para realizar o login.
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.formArea}>
+                <div className={styles.inputArea}>
+                    <InputField
+                        color={data.tenant.mainColor}
+                        placeholder="Digite seu e-mail"
+                        value={email}
+                        onChange={setEmail}
+                    />
+                </div>
+
+                <div className={styles.inputArea}>
+                    <InputField
+                        color={data.tenant.mainColor}
+                        placeholder="Digite sua senha"
+                        value={password}
+                        onChange={setPassword}
+                        password
+                    />
+                </div>
+
+                <div className={styles.inputArea}>
+                    <Button
+                        color= {data.tenant.mainColor}
+                        label="Entrar"
+                        onClick={handleSubmit}
+                        fill
+                    />
+                </div>
+            </div>
+
+            <div 
+                className={styles.forgetArea}
+                style={{borderBottomColor: data.tenant.mainColor}}
+                >
+                Esqueceu sua senha? <Link href={`/${data.tenant.slug}/forget`}><a style={{color : data.tenant.mainColor}}>Clique aqui</a></Link>
+            </div>
+            <div className={styles.line}></div>
+            <div className={styles.signupArea}>
+                <Button
+                    color= {data.tenant.mainColor}
+                    label="Quero me cadastrar"
+                    onClick={handleSignUp}
+                    
+                />
+            </div>
+
         </div>
     );
 }
